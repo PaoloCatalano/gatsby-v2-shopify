1e244bf495e91ed0a319ece89ed6437e638a8617
Paolo Catalano <paolo12587@gmail.com>
2021-05-25 12:00:05 +0200

    primo commit

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Layout,
+  SEO,
+  HomepageCollectionsGrid,
+  FeaturedProducts,
+} from 'components';
+import ProductContext from 'context/ProductContext';
+
+const IndexPage = () => {
+  const { collections } = React.useContext(ProductContext);
+
+  return (
+    <Layout>
+      <SEO description="The MadHatter store homepage" title="Homepage" />
+      <HomepageCollectionsGrid
+        collections={
+          collections.filter(collection => collection.title !== 'Featured') ||
+          []
+        }
+      />
+      {!!collections.find(collection => collection.title === 'Featured') && (
+        <FeaturedProducts />
+      )}
+    </Layout>
+  );
+};
+
+export default IndexPage;
